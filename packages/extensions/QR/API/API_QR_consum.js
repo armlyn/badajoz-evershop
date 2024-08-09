7726d11920bb3c2b8467350b55f131b9ff2229fd
Darwin Alejandro <darwin.elegiga02@gmail.com>
2024-08-09 11:48:37 -0400

    correctionName

--- packages/extensions/QR/API/API_QR_consum.js
+++ packages/extensions/QR/API/API_QR_consum.js
@@ -1,9 +1,9 @@
-const { getEnv } = require('@evershop/evershop/src/lib/util/getEnv');
+// const { getEnv } = require('@evershop/evershop/src/lib/util/getEnv');
 
 
 export const fetchData = async (text) => {
-    const apiKey = getEnv('QR_API', '');
-    console.log(apiKey)
+    // const apiKey = getEnv('QR_API', '');
+    // console.log(apiKey)
 
     const url = `https://quickchart.io/qr?text=${text}&size=500&format=png`;
     return url
