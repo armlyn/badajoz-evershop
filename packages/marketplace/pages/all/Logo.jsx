1e6f15b77e35851139593bafa000bab6d8afa1cb
Rut Maria Martinez <152558337+Ruki-04@users.noreply.github.com>
2024-08-16 06:32:48 -0400

    Create marketplace theme and add logo images

---
+++ packages/marketplace/pages/all/Logo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../scss/global.scss';
+
+function Logo() {
+  return (
+    <div>
+      <a href="/">
+        <img
+          className="w-200"
+          src="/escudo_2016_ancho.svg"
+          alt="Logo de Badajoz"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default Logo;
+
+export const layout = {
+  areaId: 'header',
+  sortOrder: 5
+};
