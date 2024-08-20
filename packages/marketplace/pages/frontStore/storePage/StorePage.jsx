c3dd70502471aa015ea0c44835338a57d5523684
Armando Carracedo <armando@lynsoluciones.com>
2024-08-20 15:24:44 +0200

    wip: fix title

--- packages/marketplace/pages/frontStore/storePage/StorePage.jsx
+++ packages/marketplace/pages/frontStore/storePage/StorePage.jsx
@@ -1,50 +1,51 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import StoreList from './StoreList';
+import './StorePage.scss';
 
 export default function StorePage({
-    stores: { items: stores, total, currentFilters = [] }
+  stores: { items: stores, total, currentFilters = [] }
 }) {
-    
-    return (
 
-        <div className="page-width">
-            <h3 className="uppercase h5 tracking-widest">
-                Tiendas
-            </h3>
-            <StoreList stores={stores} countPerRow={4} />
-        </div>
-    );
+  return (
+
+    <div className="page-width">
+      <h1 className="store__name">
+        Tiendas
+      </h1>
+      <StoreList stores={stores} countPerRow={4} />
+    </div>
+  );
 }
 
 StorePage.propTypes = {
-    stores: PropTypes.shape({
-        items: PropTypes.arrayOf(
-            PropTypes.shape({
-                storeId: PropTypes.number,
-                uuid: PropTypes.string,
-                name: PropTypes.string,
-                address: PropTypes.string,
-                status: PropTypes.number,
-                editUrl: PropTypes.string,
-                updateApi: PropTypes.string,
-                deleteApi: PropTypes.string
-            })
-        ),
-        total: PropTypes.number,
-        currentFilters: PropTypes.arrayOf(
-            PropTypes.shape({
-                key: PropTypes.string,
-                operation: PropTypes.string,
-                value: PropTypes.string
-            })
-        )
-    }).isRequired
+  stores: PropTypes.shape({
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        storeId: PropTypes.number,
+        uuid: PropTypes.string,
+        name: PropTypes.string,
+        address: PropTypes.string,
+        status: PropTypes.number,
+        editUrl: PropTypes.string,
+        updateApi: PropTypes.string,
+        deleteApi: PropTypes.string
+      })
+    ),
+    total: PropTypes.number,
+    currentFilters: PropTypes.arrayOf(
+      PropTypes.shape({
+        key: PropTypes.string,
+        operation: PropTypes.string,
+        value: PropTypes.string
+      })
+    )
+  }).isRequired
 };
 
 export const layout = {
-    areaId: 'content',
-    sortOrder: 15
+  areaId: 'content',
+  sortOrder: 15
 };
 
 export const query = `
