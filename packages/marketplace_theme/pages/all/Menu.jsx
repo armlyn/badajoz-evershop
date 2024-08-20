978f0e71bef66888ab3b4c2df36620c634fc4cb6
Rut Maria Martinez <152558337+Ruki-04@users.noreply.github.com>
2024-08-20 09:33:06 -0400

    Implement toggle button for navbar

--- packages/marketplace_theme/pages/all/Menu.jsx
+++ packages/marketplace_theme/pages/all/Menu.jsx
@@ -1,11 +1,11 @@
-import PropTypes from "prop-types";
-import React from "react";
+import PropTypes from 'prop-types';
+import React from 'react';
 
 export default function Menu({ menu: { items } }) {
-    return (
-        <div className="main-menu self-center hidden md:block">
-            <ul className="nav flex space-x-275 justify-content-center">
-                {/* {items.map((i, index) => (
+  return (
+    <div className="main-menu self-center hidden md:block">
+      <ul className="nav flex space-x-275 justify-content-center">
+        {/* {items.map((i, index) => (
                     // eslint-disable-next-line react/no-array-index-key
                     <li className="nav-item" key={index}>
                         <a className="nav-link hover:underline" href={i.url}>
@@ -13,35 +13,35 @@ export default function Menu({ menu: { items } }) {
                         </a>
                     </li>
                 ))} */}
-                <li className="nav-item">
-                    <a className="nav-link hover:underline" href={"/regions"}>
-                        Municipios
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link hover:underline" href={"/stores"}>
-                        Tiendas
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+        <li className="nav-item">
+          <a className="nav-link hover:underline" href="/regions">
+            Municipios
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link hover:underline" href="/stores">
+            Tiendas
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 Menu.propTypes = {
-    menu: PropTypes.shape({
-        items: PropTypes.arrayOf(
-            PropTypes.shape({
-                name: PropTypes.string.isRequired,
-                url: PropTypes.string.isRequired,
-            })
-        ).isRequired,
-    }).isRequired,
+  menu: PropTypes.shape({
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired
+      })
+    ).isRequired
+  }).isRequired
 };
 
 export const layout = {
-    areaId: "header",
-    sortOrder: 5,
+  areaId: 'header',
+  sortOrder: 50
 };
 
 export const query = `
