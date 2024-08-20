c690074311a738c901e0125606db59112327badb
Rut Maria Martinez <152558337+Ruki-04@users.noreply.github.com>
2024-08-20 09:33:06 -0400

    Implement toggle button for navbar

--- packages/marketplace_theme/pages/all/Layout.jsx
+++ packages/marketplace_theme/pages/all/Layout.jsx
@@ -5,27 +5,36 @@ import './Layout.scss';
 import './tailwind.scss';
 
 export default function Layout() {
-    return (
-        <>
-            <LoadingBar />
-            <div className="header flex justify-between">
-                <Area
-                    id="header"
-                    noOuter
-                    coreComponents={[]}
-                />
-            </div>
-            <main className="content">
-                <Area id="content" className="" noOuter />
-            </main>
-            <div className="footer">
-                <Area id="footer" noOuter coreComponents={[]} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <LoadingBar />
+      <div className="header flex justify-between">
+        <Area
+          id="header"
+          noOuter
+          coreComponents={[
+            {
+              component: { default: Area },
+              props: {
+                id: 'icon-wrapper-toggle',
+                className: 'icon-wrapper flex justify-between space-x-1'
+              },
+              sortOrder: 20
+            }
+          ]}
+        />
+      </div>
+      <main className="content">
+        <Area id="content" className="" noOuter />
+      </main>
+      <div className="footer">
+        <Area id="footer" noOuter coreComponents={[]} />
+      </div>
+    </>
+  );
 }
 
 export const layout = {
-    areaId: 'body',
-    sortOrder: 1
+  areaId: 'body',
+  sortOrder: 1
 };
